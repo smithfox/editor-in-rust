90181c523304e1c686604ec25ecc3027b82925ee
smith <smithfox@qq.com>
2016-11-22 17:16:31 +0800

    gui hello world

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # editor-in-rust
 在Windows下用Rust语言编写一个编辑器。
 
-先要安装好rust gun windows 64bit 版本
+先要安装好Rust Windows (GNU ABI) (.msi) 64bit 版本
 
 
 我的项目目录为 /e/git/editor-in-rust
@@ -17,4 +17,14 @@ $cargo new project --bin
 直接运行就可以显示: Hello, World!
 ```
 $cargo run
+```
+
+编辑 /e/git/editor-in-rust/project/src/main.rs 为一个 GUI 版本的 Hello World!
+
+需要添加 [winapi](https://crates.io/crates/winapi), [user32](https://crates.io/crates/user32-sys) 外部库
+
+在 Cargo.toml 的 dependencies 下添加
+```
+winapi = "0.2.8"
+user32-sys = "0.2.0"
 ```
--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,17 @@
+extern crate winapi;
+extern crate user32;
+use std::ffi::OsStr;
+use std::io::Error;
+use std::iter::once;
+use std::os::windows::ffi::OsStrExt;
+use std::ptr::null_mut;
 fn main() {
-    println!("Hello, world!");
+    let msg = "Hello, world!";
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+    let ret = unsafe {
+        user32::MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), winapi::MB_OK)
+    };
+    if ret == 0 {
+        println!("Failed: {:?}", Error::last_os_error());
+    }
 }
